40a60cd57288904d8b6057041777270bc532c386
nishekh-er-vst-au4 <nishekhgowda95@gmail.com>
2020-06-25 12:09:50 +0530

    update

--- index.js
+++ index.js
@@ -36,14 +36,11 @@ app.get("/tweets", async (req, res) => {
     });
   }
 });
-//serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static("client/build"));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(5000, () => console.log("App running at localhost:5000 🚀"));
